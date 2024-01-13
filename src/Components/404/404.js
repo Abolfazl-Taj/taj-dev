567f3c932558ca015b428e069f85bc49228262f2
Abolfazl-Taj <leviackermanam18@gmail.com>
2024-01-13 16:40:46 +0330

    buges fixed

--- src/Components/404/404.js
+++ src/Components/404/404.js
@@ -8,8 +8,10 @@ function NotFound(){
       <div className="notfound">
         
             <img src={notaks} alt="" />
+            <div className="Not-Found">
             <h1 className="not-header">Are U Ready To Visit My Site ?</h1>
             <Link to='/' className="links notb">Go Home</Link>
+            </div>
 
 </div>
     
