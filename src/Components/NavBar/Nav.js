69a91114694ee81aee94f84492715ea83cfbdebf
Abolfazl-Taj <leviackermanam18@gmail.com>
2024-01-21 14:50:45 +0330

    updates

--- src/Components/NavBar/Nav.js
+++ src/Components/NavBar/Nav.js
@@ -1,26 +1,46 @@
 import { useEffect , useState} from 'react';
 import './Nav.css';
 import { NavLink } from 'react-router-dom';
-function Nav({navbar}){
+function Nav({navbar , darkMode}){
   let [isopen , setIsopen] = useState(false)
+   useEffect(()=>{
+      console.log(1)
+   })
   useEffect(()=>{
     setIsopen(navbar)
   } , [navbar])
   const clickhandeler = ()=>{
     setIsopen(false)
   }
+   if(darkMode === true){
     return(
-        <div className={isopen=== true ? "nav open" : "nav"} >
-          {isopen ===true && <button className='MenuClose' onClick={clickhandeler}> <i class="fa-solid fa-xmark"></i> </button>}
-          
-          
-          <NavLink to='/' className="links">Home</NavLink>
-          <NavLink to='/Interdouce' className="links">Intrdouce </NavLink>
-          <NavLink to='/Contact' className="links">Contact</NavLink>
-          <NavLink to='/Skills' className="links">Skills</NavLink>
-          <NavLink to='/Projects' className="links">Projects</NavLink>
+      <div className={isopen=== true ? "nav open dark-nav" : "nav dark-nav"}>
+        {isopen ===true && <button className='MenuClose' onClick={clickhandeler}> <i class="fa-solid fa-xmark"></i> </button>}
+        
+        
+        <NavLink to='/' className="links">Home</NavLink>
+        <NavLink to='/Interdouce' className="links">Intrdouce </NavLink>
+        <NavLink to='/Contact' className="links">Contact</NavLink>
+        <NavLink to='/Skills' className="links">Skills</NavLink>
+        <NavLink to='/Projects' className="links">Projects</NavLink>
+        <NavLink to='/Exampels' className="links">Exampels</NavLink>
+      </div>
+  )
+   }else{
+    return(
+      <div className={isopen=== true ? "nav open" : "nav"}>
+        {isopen ===true && <button className='MenuClose' onClick={clickhandeler}> <i class="fa-solid fa-xmark"></i> </button>}
+        
+        
+        <NavLink to='/' className="links">Home</NavLink>
+        <NavLink to='/Interdouce' className="links">Intrdouce </NavLink>
+        <NavLink to='/Contact' className="links">Contact</NavLink>
+        <NavLink to='/Skills' className="links">Skills</NavLink>
+        <NavLink to='/Projects' className="links">Projects</NavLink>
+        <NavLink to='/Exampels' className="links">Exampels</NavLink>
+      </div>
+  )
+   }
 
-        </div>
-    )
 }
 export default Nav
